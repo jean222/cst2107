8cfda1bdf4ac3760d697b677442116a71b31c6d2
Jean <105907450+jean222@users.noreply.github.com>
2023-05-27 15:25:55 -0700

    started assignemnt1

---
+++ Assignments/Assignment1/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+//import React from 'react'
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className='header-container'>
+        <img src='https://upload.wikimedia.org/wikipedia/hif/f/ff/Manchester_United_FC_crest.png' width={'40px'} />
+        <h1>Manchester United</h1>
+    </header>
+  )
+}
