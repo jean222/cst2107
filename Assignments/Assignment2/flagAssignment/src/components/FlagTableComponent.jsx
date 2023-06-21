329f520331a8bab18775354f22d257d08c9fb594
jean222 <105907450+jean222@users.noreply.github.com>
2023-06-20 18:12:09 -0700

    .

--- Assignments/Assignment2/flagAssignment/src/components/FlagTableComponent.jsx
+++ Assignments/Assignment2/flagAssignment/src/components/FlagTableComponent.jsx
@@ -1,18 +1,47 @@
-import { TableContainer, Table, TableHead, TableCell, TableRow, Grid} from '@mui/material';
+import { TableContainer, Table, TableHead, Box, TableCell, TableRow, Grid, TableBody} from '@mui/material';
 
 
 const FlagTableComponent = ({ data }) => {
     return(
+        <TableContainer >
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Coin</TableCell>
+                        <TableCell>Symbol</TableCell>
+                        <TableCell>Price</TableCell>
+                        <TableCell>Price Change</TableCell>
+                    </TableRow>
+                </TableHead>
+
+                <TableBody>
+                    {
+                        data.map((flagCountry) => {
+                            return <TableRow>
+                                <TableCell>
+                                    <Box >
+                                    <img width={40} src={flagCountry.flags.png} />
+                                    </Box>
+                                </TableCell>
+                            </TableRow>
+                        }) 
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>        
         
-        data.map((countryFlag) => {
-            return <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                <Grid item xs={2} sm={4} md={4} key={index}>
-                    <Item>HELLO</Item>
-                </Grid>
+        // data.map((countryFlag) => {
+        //     return <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        //         <Grid item xs={2} sm={4} md={4} key={index}>
+        //             <Item>HELLO</Item>
+        //         </Grid>
                 
-            </Grid>
-        })
-    )
+        //     </Grid>
+        // })
+
+        
+        
+    ) 
 
 }
 
