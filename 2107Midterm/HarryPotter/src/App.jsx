61cf187eacce63dd0e7bd3dfa45af209a4a085ba
Jean <105907450+jean222@users.noreply.github.com>
2023-06-29 22:55:07 -0700

    useRoutes use

--- 2107Midterm/HarryPotter/src/App.jsx
+++ 2107Midterm/HarryPotter/src/App.jsx
@@ -1,13 +1,41 @@
 import { useRoutes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage/HomePage'
+import Gryffindor from './pages/Gryffindor/Gryffindor'
+import Hufflepuff from './pages/Hufflepuff/Hufflepuff'
+import Ravenclaw from './pages/Ravenclaw/Ravenclaw'
+import Slytherin from './pages/Slytherin/Slytherin'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 
 function App() {
-  let house = useRoutes([
+  let element = useRoutes([
     {
       path: '/',
-      house: 
+      element: <HomePage />
+    },
+    {
+      path: '/gryffindor',
+      element: <Gryffindor />
+    },
+    {
+      path: '/hufflepuff',
+      element: <Hufflepuff />
+    },
+    {
+      path: '/ravenclaw',
+      element: <Ravenclaw />
+    },
+    {
+      path: '/slytherin',
+      element: <Slytherin />
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />
     }
   ])
+
+  return element;
 }
 
 export default App
