4116badc503c883dd5ce34805dfd9988dc9f3146
Jean <105907450+jean222@users.noreply.github.com>
2023-06-29 22:46:47 -0700

    jsx files created

---
+++ 2107Midterm/HarryPotter/src/pages/Gryffindor/Gryffindor.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Gryffindor() {
+  return (
+    <div>Gryffindor</div>
+  )
+}
---
+++ 2107Midterm/HarryPotter/src/pages/Hufflepuff/Hufflepuff.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Hufflepuff() {
+  return (
+    <div>Hufflepuff</div>
+  )
+}
