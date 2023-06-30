7e4f09937f6e8e8aa1bc32abeb0d9ae9ca9b4d64
Jean <105907450+jean222@users.noreply.github.com>
2023-06-30 00:15:40 -0700

    buttons done and theyre wokring

--- 2107Midterm/HarryPotter/src/pages/HomePage/HomePage.jsx
+++ 2107Midterm/HarryPotter/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import Slytherin from '../Slytherin/Slytherin'
+import { ButtonGroup, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom'
 
 export default function HomePage() {
+
+  const navigate = useNavigate();
+
+  const navigateGryffindor= () => {
+    navigate('/gryffindor');
+  }
+
+  const navigateHufflepuff = () => {
+    navigate('hufflepuff');
+  }
+
+  const navigateRavenclaw = () => {
+    navigate('ravenclaw');
+  }
+
+  const navigateSlytherin = () => {
+    navigate('slytherin');
+  }
+ 
   return (
-    <div>HomePagehvj</div>
+    <>
+    <ButtonGroup size="large" variant="text" aria-label="text button group">
+        <Button onClick={navigateGryffindor}>Gryffindor</Button>
+        <Button onClick={navigateHufflepuff}>Hufflepuff</Button>
+        <Button onClick={navigateRavenclaw}>Ravenclaww</Button>
+        <Button onClick={navigateSlytherin}>Slytherin</Button>
+    </ButtonGroup>
+    </>
   )
 }
