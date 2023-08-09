a5dbb6f5cc205002de2dac3522b794dcdf4001d3
jean222 <105907450+jean222@users.noreply.github.com>
2023-08-09 13:44:36 -0700

    files not needed deleted

--- Assignments/Assignment2/flagAssignment/src/compnents/DetailsContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function DetailsContainer() {
-  return (
-    <div>DetailsContainer</div>
-  )
-}
--- Assignments/Assignment2/flagAssignment/src/pages/NotFoundPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NotFoundPage() {
-  return (
-    <div>Page Is Not Found!</div>
-  )
-}
